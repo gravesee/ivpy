6efa4c6d948a2af53d55dd5ed503fcd781e9ac21
Zelazny7 <gravcon5@gmail.com>
2019-12-05 21:10:18 -0600

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ sources = [f"src/{x}.c" for x in ['_core', 'variable', 'xtab', 'queue']]
 
 extension = Extension('_core', sources=sources, include_dirs=[numpy.get_include(), 'src'])
 
-setup(name='ivypy',
+setup(name='ivpy',
       version='0.1',
       description='Discretize continuous arrays using information value.',
       ext_modules=[extension])
