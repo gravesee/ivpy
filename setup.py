38658084f05edb3325d1c87adfb15313a821eb64
Graves <GravesEE@risk.regn.net>
2020-03-16 14:52:44 -0500

    fixed include dirs

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages
 from distutils.core import setup, Extension
 import numpy
 
-sources = [f"src/{x}.c" for x in ['_core', 'variable', 'xtab', 'queue']]
+sources = ["src/_core.c", "src/queue.c", "src/variable.c", "src/xtab.c"]
 
-extension = Extension('_core', sources=sources, include_dirs=[numpy.get_include(), 'src'])
+extension = Extension('_core', sources=sources, include_dirs=[numpy.get_include(), 'pycard/src'])
 
 setup(name='ivpy',
       version='0.1',
